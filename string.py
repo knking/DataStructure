e9d9edc82b8c7c6cd6fc33597c6def3767a187f4
knking <knr020498@gmail.com>
2020-07-25 05:11:49 +0530

    stpalind

--- string.py
+++ string.py
@@ -101,11 +101,30 @@
 #)9.Write  a program  to reverse each word	in	the	given string.
 # def rev(s):
 #     w=s.split(" ")
-#     nw=[i[::]for i in w]
+#     nw=[i[::-1]for i in w]
 #     ns=" ".join(nw)
 #     return ns
-# s1="king"
+# s1="king krishna"
 # print(rev(s1))
+
+def reverserWords(string):
+      st = list()
+      for i in range(len(string)):
+            if string[i] != " ":
+                  st.append(string[i])
+            else:
+                  while len(st) > 0:
+                        print(st[-1], end="")
+                        st.pop()
+                  print(end=" ")
+
+      while len(st) > 0:
+            print(st[-1], end="")
+            st.pop()
+
+string="geeks for"
+reverserWords(string)
+
 ##10) program to check if a string is palindrome or not
 
 string=input(("Enter a string:"))
@@ -113,3 +132,15 @@ if(string==string[::-1]):
       print("The string is a palindrome")
 else:
       print("Not a palindrome")
+
+num=int(input("Enter a number:"))
+temp=num
+rev=0
+while(num>0):
+    dig=num%10
+    rev=rev*10+dig
+    num=num//10
+if(temp==rev):
+    print("The number is palindrome!")
+else:
+    print("Not a palindrome!")
