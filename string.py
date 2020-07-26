f88ae5b8bfe5f698651e6daf59522dc45299e825
knking <knr020498@gmail.com>
2020-07-26 11:09:33 +0530

    strmore

--- string.py
+++ string.py
@@ -1,3 +1,4 @@
+
 ##)1. Write a basic program to take input (String)	from User and just print it.
 # s=input("Enter string : ")
 # print("you entered-->",s)
@@ -145,27 +146,91 @@
 # else:
 #     print("Not a palindrome!")
 
-#11.Write a Code to check whether one string is a rotation	of another
+# #11.Write a Code to check whether one string is a rotation	of another
+#
+# def check_rotation(s1,s2):
+#      size1=len(s1)
+#      size2=len(s2)
+#      if size1!=size2:
+#            return 0
+#      temp=s1+s2
+#      if temp.count(s2)>0:
+#            return 1
+#      else:
+#            return 0
+# s1="abcd"
+# s2="acda"
+# if check_rotation(s1,s1):
+#       print("String are rotation of each other")
+# else:
+#       print("Strig are not rotation of each other")
 
-def check_rotation(s1,s2):
-     size1=len(s1)
-     size2=len(s2)
-     if size1!=size2:
-           return 0
-     temp=s1+s2
-     if temp.count(s2)>0:
-           return 1
-     else:
-           return 0
-s1="abcd"
-s2="acda"
-if check_rotation(s1,s1):
-      print("String are rotation of each other")
-else:
-      print("Strig are not rotation of each other")
+
+# #12)Write a program to	remove	Duplicate characters from the String
+# s="geeksforgeeks"
+# s=" ".join(set(s))
+# print(s.replace(" ",""))
+
+# s="krishnanand"
+# i=0
+# s1=""
+# for x in s:
+#       if s.index(x)==i:
+#             s1+=x
+#       i+=1
+# print(s1)
+
+##13) Interleaving of two given strings with no common characters
+
+# def isInterleaving(a,b,c):
+#     i=0
+#     j=0
+#     k=0
+#     while k !=len(c)-1:
+#         if i< len(a) and a[i]==c[k]:
+#             i+=1
+#         elif j < len(b) and b[j]==c[k]:
+#             j+=1
+#         else:
+#             return 0
+#         k+=1
+#     if a[i-1] or b[j-1]:
+#         return 0
+#     return 1
+# a="ab"
+# b="cd"
+# c="abcd"
+# if isInterleaving(a,b,c)==1:
+#     print("Interleaving")
+# else:
+#     print("Not interleaving")
+
+# interiving of strinng
+# s1='aaabb'
+# s2='cd'
+# s=list(s2)
+#
+# for i,c in enumerate(s1):
+#     s.insert(i*2,c)
+# print(''.join(s))
 
 
-#12)Write a program to	remove	Duplicate characters from the String
-s="geeksforgeeks"
-s=" ".join(set(s))
-print(s.replace(" ",""))
+
+def pal(a,s,e):
+    while s <=e:
+        if a[s]==a[e]:
+            s+=1
+            e-=1
+            return True
+        else:
+            return False
+a='aabbaa'
+s=0
+e=len(a)-1
+
+print(pal(a,s,e))
+
+
+
+
+
